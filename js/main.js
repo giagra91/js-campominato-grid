9850669879c9a80c0b71ab68d10ce9a621a9287e
giagra91 <giagra91@gmail.com>
2022-03-07 15:09:54 +0100

    inserimento funzioni

--- js/main.js
+++ js/main.js
@@ -2,31 +2,13 @@
 const userChoice = document.getElementById(`game-option`);
 const gridElement = document.getElementById(`grid`); 
 
+let numberGrids = 0;
+
 // Aggiungo la funzione di click per stampare nel DOM gli elementi in base alla scelta dell'utente
 document.getElementById(`play`).addEventListener(`click`, function(){
     document.getElementById(`play`).classList.add(`d-none`);
 
-    let numberGrids = 0;
-
-    if (userChoice.value == `easy`){
-        numberGrids = 100;
-    } else if (userChoice.value == `medium`){
-        numberGrids = 80;
-    } else if (userChoice.value == `hard`){
-        numberGrids = 50;
-    }
-
-    for (i = 1 ; i < numberGrids + 1 ; i++){
-        let newDiv = document.createElement(`div`);
-        newDiv.classList.add(`box`);
-        newDiv.innerHTML+= i;
-        gridElement.appendChild(newDiv);
-
-        // Aggiungo la funzione di click per attivare e disattivare la classe box-blue
-        newDiv.addEventListener(`click`, function(){
-            newDiv.classList.toggle(`box-blue`)
-        })
-    }
+    createGame();
 
 })
 
@@ -36,3 +18,46 @@ document.getElementById(`reset`).addEventListener(`click`, function(){
     document.getElementById(`play`).classList.remove(`d-none`)
 
 })
+
+function createGame (){
+    userChoice.value;
+
+    if (userChoice.value == `easy`){
+        numberGrids = 100;
+    } else if (userChoice.value == `medium`){
+        numberGrids = 81;
+    } else if (userChoice.value == `hard`){
+        numberGrids = 49;
+    }
+
+    for (i = 1 ; i < numberGrids + 1 ; i++){
+        const createGrid = createNewBox (i);
+        gridElement.appendChild(createGrid);
+    } 
+
+}
+
+
+function createNewBox (number){
+    let newDiv = document.createElement(`div`);
+    newDiv.innerHTML= number;
+
+    if (userChoice.value == `easy`){
+        numberGrids = 100;
+        newDiv.classList.add(`box`);
+    } else if (userChoice.value == `medium`){
+        numberGrids = 81;
+        newDiv.classList.add(`box-1`);
+    } else if (userChoice.value == `hard`){
+        numberGrids = 49;
+        newDiv.classList.add(`box-2`);
+    }
+
+    // Aggiungo la funzione di click per attivare e disattivare la classe box-blue
+    newDiv.addEventListener(`click`, function(){
+        newDiv.classList.toggle(`box-blue`)
+    })
+
+    return newDiv;
+
+} 
